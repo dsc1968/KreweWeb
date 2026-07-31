eda6aa8664f00d8fd8fa8f618dc20bff79dd5a39
Doug Cobb <dougscobb@gmail.com>
2026-07-31 16:42:16 -0500

    Cleanup

--- frontend/backend/routes/health.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticateToken } = require('../middlewares/auth');
-const { upload } = require('../utils/files');
-const { get__api_status } = require('../controllers/healthController');
-
-router.get('/api/status', get__api_status);
-
-module.exports = router;
